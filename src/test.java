a9b7f8e4f29b31f4c557fb328b56a10bd1c374fe
Aditya Nishad <Adityanishad0001@gmail.com>
2024-06-04 12:40:36 +0530

    Adding new static method

--- src/test.java
+++ src/test.java
@@ -1,7 +1,18 @@
 public class test {
+
+   public static int add(int a , int b){
+
+       int c= a+b;
+       return c;
+
+   }
     public static void main(String[] args) {
 
 
         System.out.println("testing github ");
+
+
+        System.out.println(add(33,4));
+
     }
 }
